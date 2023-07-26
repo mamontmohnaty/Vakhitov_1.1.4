2f4e294f9a1bbf57d57ae8dea794cc5902f13dac
Ravil Vakhitov <mamontmohnaty@gmail.com>
2023-07-26 21:14:36 +0300

    add util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+
+    private static final String db_driver = "com.mysql.cj.jdbc.Driver";
+    private static final String db_url = "jdbc:mysql://localhost:3306/db_kata_study";
+    private static final String db_username = "bestuser";
+    private static final String db_password = "bestuser";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(db_driver);
+            connection = DriverManager.getConnection(db_url, db_username, db_password);
+            System.out.println("Connection OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
 }
