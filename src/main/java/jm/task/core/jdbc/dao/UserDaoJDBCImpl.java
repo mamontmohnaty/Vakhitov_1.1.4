102531b2087367743030a161b4554ac95e5b6e0f
Ravil Vakhitov <mamontmohnaty@gmail.com>
2023-07-26 22:16:49 +0300

    user_dao

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,23 +1,34 @@
 package jm.task.core.jdbc.dao;
 
+import jm.task.core.jdbc.util.Util;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
 
+    Connection connection = getConnection();
+
     public void createUsersTable() {
 
-    }
 
+
+    }
     public void dropUsersTable() {
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+
+
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT "
 
     }
 
